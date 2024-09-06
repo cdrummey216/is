7fa7a95739f815bfc520f9c8749b3c3d0bc0472b
caveman <46973458+cdrummey216@users.noreply.github.com>
2024-09-06 17:45:37 -0400

    Add files via upload

--- is.sh
+++ is.sh
@@ -1,29 +1,65 @@
 #!/bin/bash
 # Game of Is: What is and what may ever be?
+# answer questions and return an answer with altering odds
 read -p "Welcome to the game of is! - Who are you? " name
-read -p "Well met, {$name}. I am the spirit in the shell. I am the oneshot oracle living in your computer. First, I bid you answer my questions-three. Who do you serve? 1/0 " q0
-read -p "I see, {$name}. What is your fate? 1/0 " q1
-read -p "I understand, {$name}. How did this begin? 1/0 " q2
+echo "I am the spirit in the shell."
+echo "I am the oneshot oracle living in your computer."
+echo "I bid you answer my questions-three, so that I may calculate an answer to your any query."
+while true; do
 
-if [ "$q0" = "1" ]; then
-  echo "Ok."
-fi
-if [ "$q0" = "0" ]; then
-  echo "Goodbye."
-fi
+echo "###############|````|##############"
+echo "###############|````|##############"
+echo "###############|````|##############"
+echo "###############|````|##############"
+echo "###############|````|##############"
+echo "###############|````|##############"
+echo "###############|````|##############"
+echo "###############|````|##############"
+echo "###############|````|##############"
+echo "###############|````|##############"
+echo "###############|````|##############"
+echo "###############|````|##############"
+echo "###############|````|##############"
+echo "###############|````|##############"
+echo "#####\```````````````````````/#####"
+echo "######\`````````````````````/######"
+echo "#######\```````````````````/#######"
+echo "########\`````````````````/########"
+echo "#########\```````````````/#########"
+echo "##########\`````````````/##########"
+echo "###########\```````````/###########"
+echo "############\`````````/############"
+echo "#############\```````/#############"
+echo "##############\`````/##############"
+echo "###############\```/###############"
+echo "################\`/################"
+echo "#################\/################"
+echo ""
 
-if [ "$q1" = "1" ]; then
-  echo "Ok."
-fi
-if [ "$q1" = "0" ]; then
-  echo "Ok."
-fi
+clock=21
 
-if [ "$q2" = "1" ]; then
-  echo "Ok."
+read -p "Well met, $name. First, tell me, are you alive now? y/n " q0
+if [ "$q0" = "y" ]; then
+  clock=21
+  read -p "That is good. Next, are you mortal here? y/n  " q1
+  if [ "$q1" = "y" ]; then
+      read -p "Ever better, $name. Last, is your spirit eternal? y/n " q2
+
+      if [ "$q2" = "y" ]; then
+        clock=10
+      fi
+      if [ "$q2" = "n" ]; then
+        clock=21
+      fi
+  fi
+  if [ "$q1" = "n" ]; then
+      clock=21
+      read -p "What's your secret?" secret
+  fi
 fi
-if [ "$q2" = "0" ]; then
-  echo "Ok."
+if [ "$q0" = "n" ]; then
+  clock=21
+  read -p "What's your secret?" secret
 fi
 
 answers=("It is certain." "It is decidedly so."
@@ -34,15 +70,16 @@ answers=("It is certain." "It is decidedly so."
   "Better not tell you now." "Cannot predict now."
   "Concentrate and ask again." "Don't count on it."
   "My reply is no." "My sources say no."
-  "Outlook not so good." "Very doubtful.")
+  "Outlook not so good." "Very doubtful." "No.")
 
 read -p "What is your question? " question
 
-answer=$(( $RANDOM % 20 ))
+answer=$(( $RANDOM % clock ))
 
 echo ""
 echo ""
 echo "I have looked into the future and I say: "
 echo "     ${answers[$answer]}" ; echo ""
 
-exit 0
+#exit 0
+done
