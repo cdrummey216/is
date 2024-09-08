72d0206fae7604f0bb2c6a026c8d51e605a62305
caveman <46973458+cdrummey216@users.noreply.github.com>
2024-09-08 13:07:40 -0400

    Add files via upload

--- is.sh
+++ is.sh
@@ -1,36 +1,57 @@
 #!/bin/bash
 # Game of Is: What is and what may ever be?
 # answer questions and return an answer with altering odds
-
-read -p "Welcome to the game of is! - Who are you? " name
+echo ""
+echo ""
+echo ""
+echo ""
+read -p "     Welcome to the game of is! - Who are you? " name
+echo ""
+echo ""
+echo "" && sleep 1
+echo ""
+echo "     Well met, $name. Let's begin."
+echo ""
+echo ""
+echo "" && sleep 1
+echo ""
 cat << "EOF"
      .-.
    .'   `.
-   :0 0   :
+   :0 0   : I am the spirit in the shell.
    : o    `.
   :         ``.
- :             `.
+ :             `.I am the oneshot oracle living in your computer.
 :  :         .   `.
-:   :          ` . `.
+:   :          ` . `.I am Gh0st-in-the-machine.
  `.. :            `. ``;
-    `:;             `:'
-       :              `.
+    `:;             `:' I bid you answer my questions-three,
+       :              `.so that I may calculate an answer to your any query.
         `.              `.     .
           `'`'`'`---..,___`;.-'
     
 EOF
-echo "I am the spirit in the shell."
-echo "I am the oneshot oracle living in your computer."
-echo "I bid you answer my questions-three, so that I may calculate an answer to your any query."
+
 
 while true; do
 clock=21
-
-read -p "Well met, $name. First, tell me, are you alive now? y/n " q0
+echo ""
+echo ""
+echo "" && sleep 1
+echo ""
+read -p "     First, tell me, are you alive now? y/n " q0
 if [ "$q0" = "y" ]; then
-  read -p "That is good. Next, are you mortal here? y/n  " q1
+  echo ""
+  echo ""
+  echo "" && sleep 1
+  echo ""
+  read -p "     That is good. Next, are you mortal here? y/n  " q1
   if [ "$q1" = "y" ]; then
-      read -p "Ever better, $name. Last, is your spirit eternal? y/n " q2
+    echo ""
+    echo ""
+    echo "" && sleep 1
+    echo ""
+      read -p "     Ever better, $name. Last, is your spirit eternal? y/n " q2
       if [ "$q2" = "y" ]; then
         min=0
         max=8
@@ -43,17 +64,25 @@ if [ "$q0" = "y" ]; then
       fi
   fi
   if [ "$q1" = "n" ]; then
-        min=13
-        max=21
-        clock=$(($RANDOM%($max-$min+1)+$min))
-      read -p "What's your secret?" secret
+     min=13
+     max=21
+     clock=$(($RANDOM%($max-$min+1)+$min))
+     echo ""
+     echo ""
+     echo "" && sleep 1
+     echo ""
+     read -p "     What's your secret?" secret
   fi
 fi
 if [ "$q0" = "n" ]; then
    min=13
    max=21
    clock=$(($RANDOM%($max-$min+1)+$min))
-   read -p "What's your secret?" secret
+   echo ""
+echo ""
+echo "" && sleep 1
+echo ""
+   read -p "    What's your secret?" secret
 fi
 
 answers=("It is certain." "It is decidedly so."
@@ -65,15 +94,62 @@ answers=("It is certain." "It is decidedly so."
   "Concentrate and ask again." "Don't count on it."
   "My reply is no." "My sources say no."
   "Outlook not so good." "Very doubtful." "No.")
-
-read -p "What is your question? " question
-
-answer=$(( $RANDOM % clock ))
-
+echo ""
+echo "" && sleep 1
 echo ""
 echo ""
-echo "I have looked into the future and I say: "
-echo "     ${answers[$answer]}" ; echo ""
+read -p "     Ask your question " question
 
+cat << "EOF"
+             ,----------------,              ,---------,
+        ,-----------------------,          ,"        ,"|
+      ,"                      ,"|        ,"        ,"  |
+     +-----------------------+  |      ,"        ,"    |
+     |  .-----------------.  |  |     +---------+      |
+     |  |       .-.       |  |  |     | -==----'|      |
+     |  |      (o o)      |  |  |     |         |      |
+     |  |      | O \      |  |  |/----|`---=    |      |
+     |  |       \   \     |  |  |   ,/|==== ooo |      ;
+     |  |        `~~~'    |  |  |  // |(((( [33]|    ,"
+     |  `-----------------'  |," .;'| |((((     |  ,"
+     +-----------------------+  ;;  | |         |,"     
+        /_)______________(_/  //'   | +---------+
+   ___________________________/___  `,
+  /  oooooooooooooooo  .o.  oooo /,   \,"-----------
+ / ==ooooooooooooooo==.o.  ooo= //   ,`\--{)B     ,"
+/_==__==========__==_ooo__ooo=_/'   /___________,"
+
+EOF
+
+echo "     You asked: $question"
+echo ""
+echo "" && sleep 1
+echo "     'I have looked into the future and I say: "
+echo "     ${answers[$clock]}'" ; echo ""
+echo "" && sleep 3
+echo "###############################################"
+echo "###############################################"
+echo "###############################################"
+echo "###############################################"
+echo "###############################################"
+echo ""
+echo ""
+echo ""
+cat << "EOF"
+     .-.
+   .'   `.
+   :0 0   :      Let's begin again.
+   : o    `.
+  :         ``.
+ :             `.
+:  :         .   `.
+:   :          ` . `.
+ `.. :            `. ``;
+    `:;             `:' 
+       :              `.
+        `.              `.     .
+          `'`'`'`---..,___`;.-'
+    
+EOF
 #exit 0
 done
